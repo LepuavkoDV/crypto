da10ff86c0e538edc9298060ddc1332e49c76529
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2021-03-10 19:51:27 +0200

    1

--- src/app/actions/data.actions.ts
+++ src/app/actions/data.actions.ts
@@ -1,15 +1,21 @@
 import { createAction, props } from '@ngrx/store';
 
-export const loadDatas = createAction(
-  '[Data] Load Datas'
+export enum DataActions {
+  loadData = '[Data] Load Data',
+  loadDataSuccess = '[Data] Load Data Success',
+  loadDataFailure = '[Data] Load Data Failure',
+}
+
+export const loadData = createAction(
+  DataActions.loadData
 );
 
-export const loadDatasSuccess = createAction(
-  '[Data] Load Datas Success',
+export const loadDataSuccess = createAction(
+  DataActions.loadDataSuccess,
   props<{ data: any }>()
 );
 
-export const loadDatasFailure = createAction(
-  '[Data] Load Datas Failure',
+export const loadDataFailure = createAction(
+  DataActions.loadDataFailure,
   props<{ error: any }>()
 );
